a54bf4105660278dcd6651aa7aefe2a84b4a63b0
CAdine <43303256+CAdine@users.noreply.github.com>
2018-11-25 17:00:50 -0800

    HighScore

--- app/src/main/java/net/androidbootcamp/quizpop/MainPage.java
+++ app/src/main/java/net/androidbootcamp/quizpop/MainPage.java
@@ -16,14 +16,8 @@ public class MainPage extends AppCompatActivity {
         Button btnPlayQuiz = findViewById(R.id.btnPlayGame);
         Button btnInstruct = findViewById(R.id.btnTutorial);
         Button btnShare = findViewById(R.id.btnShareGame);
-        Button btnScore = findViewById(R.id.btnViewScores);
 
-        btnScore.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainPage.this,HighScores.class));
-            }
-        });
+      
         btnShare.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
